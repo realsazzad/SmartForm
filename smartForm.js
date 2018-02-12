b0d55da22b454b78ca7b8765f800fcc0e213ad1c
badhannn <badhan896@gmail.com>
2018-02-12 09:28:32 +0600

    class_Work

--- smartForm.js
+++ smartForm.js
@@ -1,3 +1,58 @@
 // Your jQuery goes here
 
-var userData = {};
+var userData = {
+	name:'',
+	email:''
+};
+$('#start').click(function()
+{
+	
+	$('#welcome').hide();
+	$('#q1').show();
+});
+
+$('#q1_next').click(function(){
+	if($('#name').val() && $('#email').val())
+	{
+		userData.name=$('#name').val();
+		userData.email=$('#email').val();
+		$('#q1').hide();
+		$('#q2').show();
+	}
+	
+});
+$('#html_q').click(function()
+{
+	$('#q2').hide();
+	$('#q2a').show();
+});
+$('#css_q').click(function()
+{
+	$('#q2').hide();
+	$('#q2b').show();
+});
+$('#javascript_q').click(function()
+{
+	$('#q2').hide();
+	$('#q2c').show();
+});
+$('#html_previous').click(function()
+{
+	$('#q2a').hide();
+	$('#q2').show();
+});
+$('#html_next').click(function()
+{
+	$('#q2a').hide();
+	$('#q2b').show();
+});
+$('#css_previous').click(function()
+{
+	$('#q2b').hide();
+	$('#q2').show();
+});
+$('#css_next').click(function()
+{
+	$('#q2b').hide();
+	$('#q2c').show();
+});
