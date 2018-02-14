7e011c434f71c4928d784f4077dd57f7c34156e4
cis101 <cis101@secs.iub.edu.bd>
2018-02-14 09:30:25 +0600

    class-work-14/02

--- smartForm.js
+++ smartForm.js
@@ -2,20 +2,38 @@
 
 var userData = {
 	name:'',
-	email:''
+	email:'',
+	currentQuestion: '#welcome'
 };
+
+
+if(localStorage.getItem('userData'))
+{
+	userData=JSON.parse(localStorage.getItem('userData'));
+	
+	$('#welcome').hide();
+	$(userData.currentQuestion).show();
+	$('#name').val(userData.name);
+	$('#email').val(userData.email);
+}
+else
+{
+	localStorage.setItem('userData',JSON.stringify(userData));
+}
 $('#start').click(function()
 {
 	
 	$('#welcome').hide();
 	$('#q1').show();
+	userData.currentQuestion='#q1';
+	localStorage.setItem('userData',JSON.stringify(userData));
 });
-
 $('#q1_next').click(function(){
 	if($('#name').val() && $('#email').val())
 	{
 		userData.name=$('#name').val();
 		userData.email=$('#email').val();
+		localStorage.setItem('userData'.JSON.stringify(userData));
 		$('#q1').hide();
 		$('#q2').show();
 	}
